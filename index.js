389f4fc889abe1a8b78d1a9e0198658928081394
samuel8772 <kamauskk005@gmail.com>
2025-03-20 14:26:09 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let name of names) {
+      messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
